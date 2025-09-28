24eccb7f0c0863c5a16317b37f184a7c32b4e8a6
micahspencer-png <micahspencer@isu.edu>
2025-09-27 21:19:16 -0600

    Add project files.

---
+++ AccumulateMessagesFunction/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AccumulateMessagesFunction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
