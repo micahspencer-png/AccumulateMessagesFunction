7b1d98949cf2f85cedc2b8413e3aa523c2ec2de7
micahspencer-png <micahspencer@isu.edu>
2025-09-27 21:21:44 -0600

    set up header

--- AccumulateMessagesFunction/Program.cs
+++ AccumulateMessagesFunction/Program.cs
@@ -1,5 +1,10 @@
 ﻿namespace AccumulateMessagesFunction
 {
+    //Micah Spencer
+    //RCET2265
+    //Fall 25
+    //Accumulate Messages Function
+    //https://github.com/micahspencer-png/AccumulateMessagesFunction.git
     internal class Program
     {
         static void Main(string[] args)
