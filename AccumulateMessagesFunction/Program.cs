eccd3e27acfb0f50f129fb6f945da20183d8d4b5
micahspencer-png <micahspencer@isu.edu>
2025-09-28 18:37:18 -0600

    set up program to run program while the user doesn't quit

--- AccumulateMessagesFunction/Program.cs
+++ AccumulateMessagesFunction/Program.cs
@@ -9,7 +9,36 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Type Messages here");
+            string userMessages = "This is important";
+            string allTheMessages = "";
+            bool quit = false;
+
+            Console.WriteLine("Type Data You Want Stored. Press Q to Show All Data");
+            do
+            {
+                userMessages = Console.ReadLine();
+                if (userMessages != "q" && userMessages != "Q")
+                {
+                    allTheMessages = Messages(userMessages);
+                    Console.Clear();
+                }
+                else
+                {
+                    quit = true;
+                }
+            } while (quit == false);
+
+            Console.Clear();
+            Console.WriteLine("The Messages Typed Are:");
+            Console.WriteLine(allTheMessages);
+
+            //pause
+            Console.WriteLine("Press Enter to Quit");
+            Console.Read();
         }
+
+        private static string messages_ = "";
+
+
     }
 }
