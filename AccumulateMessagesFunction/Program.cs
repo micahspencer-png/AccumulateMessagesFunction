620d8adde77f7e2a7ed7699657d835a212962cfd
micahspencer-png <micahspencer@isu.edu>
2025-09-28 18:45:51 -0600

    program now stores data, displays data, and quits. Clear function still needs work

--- AccumulateMessagesFunction/Program.cs
+++ AccumulateMessagesFunction/Program.cs
@@ -17,27 +17,63 @@
             do
             {
                 userMessages = Console.ReadLine();
-                if (userMessages != "q" && userMessages != "Q")
+                if (userMessages == "q" || userMessages == "Q")
+                {
+                    
+                    quit = true;
+                }
+
+                else if (userMessages == "m"|| userMessages == "M")
+                {
+                    Console.Clear();
+                    Console.WriteLine("The Messages Typed Are:");
+                    Console.WriteLine(allTheMessages);
+                }
+
+                else
                 {
                     allTheMessages = Messages(userMessages);
                     Console.Clear();
                 }
-                else
-                {
-                    quit = true;
-                }
             } while (quit == false);
-
-            Console.Clear();
-            Console.WriteLine("The Messages Typed Are:");
-            Console.WriteLine(allTheMessages);
 
             //pause
             Console.WriteLine("Press Enter to Quit");
             Console.Read();
         }
 
-        private static string messages_ = "";
+        private static string _messages = "";
+
+        static string Messages(string userMessages)
+        {
+            userMessages = userMessages.Trim();
+            //grabs global
+            string messages = _messages;
+
+            if (messages.Length == 0)
+            {
+                messages += userMessages;
+            }
+
+            //skips a blank input
+            else if (userMessages != "")
+            {
+                //append the current message
+                messages += "\n" + userMessages;
+            }
+
+            //clears data if c is typed
+            else if (userMessages == "c" || userMessages == "C" )
+            {
+                messages = "";
+            }
+
+            //stores the updated data
+            _messages = messages;
+
+            //return the messages
+            return messages;
+        }
 
 
     }
